58b83af4cc519e6c975275c91d2fb9ddd62cac07
Niviera <113305331+Niviera@users.noreply.github.com>
2023-04-24 12:16:26 +0200

    Seite angebot: erstellt + finsish

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
             <a href="index.php" style="margin: 5px;"> Startseite </a>
             <a href="konto_uebersicht.php" style="margin: 5px;"> Konto </a>
             <a href="" style="margin: 5px"> Übersicht </a>
-            <a href="" style="margin: 5px"> Angebot erstellen </a>
+            <a href="angebot_erstellen.php" style="margin: 5px"> Angebot erstellen </a>
       </div>
       <div style="float: right">
             <a href="konto_erstellen.php" style="margin: 5px"> Registrieren </a>
