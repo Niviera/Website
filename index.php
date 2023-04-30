7e1083b793b6f9afbc107be6e598e2b87b0910f6
Niviera <113305331+Niviera@users.noreply.github.com>
2023-04-30 15:24:08 +0200

    changes

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <!-- Stylesheets -->
-  <link rel="stylesheet" type="text/css" href="CSS/navigation.css">
-  <link rel="stylesheet" type="text/css" href="CSS/startseite.css">
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
 
   <title>Document</title>
 </head>
 
-<body style="background-color: #dcf0ff;">
+<body>
   <!-- Header -->
   <?php
   include "Ressourcen/header.php";
@@ -22,8 +21,7 @@
   <!-- Bereich des neuesten Angebots -->
   <div style="height: 600px; display: flex; align-items: center;">
     <!-- Style Container für Angebot -->
-    <div
-      style="height: 400px; width: 800px; display: flex; margin-left:300px; border: 1px solid black; border-radius:75px">
+    <div style="height: 400px; width: 800px; display: flex; margin:auto; border: 1px solid black; border-radius:75px">
       <!-- Container Bild mit Namen-->
       <div style="width: 200px; float:left; margin:10px">
         <img src="Bilder/Lucas.jpeg" alt="Profil Bild" style="width:200px; height: 200px; border-radius:75px">
@@ -42,14 +40,14 @@
 
   <!-- Auflistung aller Angebote. -->
   <!-- Trennstrich -->
-  <div style="background-color: #74c4ff; height:30px"></div>
+  <div style="background-color: #74c4ff; height:45px"></div>
   <!-- Box für Liste Angebote -->
-  <div style="height: auto; display: flex; width: 100%">
+  <div style="height: auto; display: flex; width: 100%;">
     <!-- erweiterte Suche -->
     <div class="erweitertes_Such_Navi">
       <!-- Suche -->
       <form>
-        Suche: <input type="text">
+        <p class="suche">Suche:</p> <input type="text">
       </form>
       <ul>
         <li> <a>Garten</a> </li>
@@ -59,6 +57,54 @@
     </div>
     <!-- Angebote -->
     <div class="Container_kleine_Angebote">
+      <!-- Angebot 1 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 2 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 3 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 4 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 5 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 6 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 7 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
+      <!-- Angebot 8 -->
+      <div>
+        <p class="ueberschrift"> Hallo </p>
+        <p class="beschreibung"> Dies ist ein kleiner beispieltext zum Testen </p>
+        <p class="autor">Lucas Christoffers</p>
+      </div>
       <!-- Angebot 1 -->
       <div>
         <p class="ueberschrift"> Hallo </p>
