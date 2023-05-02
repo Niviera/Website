6bb62345496f4d499930b3c64022ee30408845cd
Niviera <113305331+Niviera@users.noreply.github.com>
2023-05-02 22:23:58 +0200

    update: Registrierung bearbeitet und section used

--- Ressourcen/header.php
+++ Ressourcen/header.php
@@ -1,34 +1,37 @@
-<div id="header" style="display:flex;background-color: #1922bd; height: 75px; align-content:center">
+<header>
+      <div id="header" style="display:flex;background-color: #1922bd; height: 75px; align-content:center">
+      </div>
+</header>
+<nav>
+      <div class="navi desktop">
+            <ul>
+                  <li><a href="index.php">Startseite</a></li>
+                  <li><a href="konto_uebersicht.php">Konto</a></li>
+                  <li><a href="uebersicht.php">Übersicht</a></li>
+                  <li><a href="angebot_erstellen.php">Angebot erstellen</a></li>
+                  <li style="float: right;"><a href="konto_erstellen.php"> Registrieren </a></li>
+                  <li style="float: right;"><a href="login.php"> Login </a></li>
+                  <li style="float: right;"><a href="index.php">Abmelden</a></li>
+            </ul>
+      </div>
 
-</div>
-<div class="navi desktop">
-      <ul>
-            <li><a href="index.php">Startseite</a></li>
-            <li><a href="konto_uebersicht.php">Konto</a></li>
-            <li><a href="uebersicht.php">Übersicht</a></li>
-            <li><a href="angebot_erstellen.php">Angebot erstellen</a></li>
-            <li style="float: right;"><a href="konto_erstellen.php"> Registrieren </a></li>
-            <li style="float: right;"><a href="login.php"> Login </a></li>
-            <li style="float: right;"><a href="index.php">Abmelden</a></li>
-      </ul>
-</div>
-
-<div class="navi mobile">
-      <a href="index.php">Startseite</a>
-      <div class="dropdownMenuPoint">
-            <button class="dropTrigger">Angebot</button>
-            <div class="dropdown-content">
-                  <a href="angebot_erstellen.php">Angebot erstellen</a>
-                  <a href="uebersicht.php">Übersicht</a>
+      <div class="navi mobile">
+            <a href="index.php">Startseite</a>
+            <div class="dropdownMenuPoint">
+                  <button class="dropTrigger">Angebot</button>
+                  <div class="dropdown-content">
+                        <a href="angebot_erstellen.php">Angebot erstellen</a>
+                        <a href="uebersicht.php">Übersicht</a>
+                  </div>
+            </div>
+            <div class="dropdownMenuPoint">
+                  <button class="dropTrigger">Konto</button>
+                  <div class="dropdown-content">
+                        <a href="konto_uebersicht.php">Eigene Daten</a>
+                        <a href="konto_erstellen.php"> Registrieren </a>
+                        <a href="login.php"> Login </a>
+                        <a href="index.php">Abmelden</a>
+                  </div>
             </div>
       </div>
-      <div class="dropdownMenuPoint">
-            <button class="dropTrigger">Konto</button>
-            <div class="dropdown-content">
-                  <a href="konto_uebersicht.php">Eigene Daten</a>
-                  <a href="konto_erstellen.php"> Registrieren </a>
-                  <a href="login.php"> Login </a>
-                  <a href="index.php">Abmelden</a>
-            </div>
-      </div>
-</div>
+</nav>
