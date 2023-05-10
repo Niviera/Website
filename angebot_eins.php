eb7f697d54071ea0577c6a167087b304238d97fe
Niviera <113305331+Niviera@users.noreply.github.com>
2023-05-10 21:55:20 +0200

    Update: Jannes sachen + error fixes

--- angebot_eins.php
+++ angebot_eins.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -18,35 +18,35 @@
     include "Ressourcen/header.php";
     ?>
     <!-- Content -->
-    <div id="angeboteins" label="angeboteins">
-        <div style="height: 500px; border: 2px solid Black">
-            <!-- Title -->
-            <h1 style="width: 100%; height: 20px; text-align: center; margin: 20px: margin-bottom: 40px"> Zu Verschenken
-            </h1>
-            <!-- Beschreibung -->
-            <p>
-                Hey dies ist mal wieder ein einfach quatsch text der ausgedacht worden ist. Denn damit soll einfach
-                getestet
-                werden
-                wie der Text tatsächlich aussieht und wie man damit herumspielen kann. Besonders wichtig ist es sobald
-                er
-                mal ein klein
-                wenig länger aussfällt. Aber irgendwie wird es immer schwerer sich quatsch auszudenken... vieleicht
-                hätte
-                ich einfach
-                versuchen sollen einen richtigen text zu schreiben, nun bin aber schon relativ weit hiermit und höre
-                bestimmt nicht auf.
-                Also setzen wir diesen sinnlosen Roman fort und das nur für den simplen grund um längere Texte mal
-                auszuprobieren.
-            </p>
-            <!-- Ersteller -->
-            <div style="height:200px;width:400px ;display: flex">
-                <!-- Bild -->
-                <img src="Bilder/profile/Lucas.jpeg" alt="Profil Bild"
-                    style="width:175px; height: 175px; border-radius:75px; float:left">
+
+    <div id="angeboteins">
+
+        <!-- Title -->
+        <div class="Container_Angebot">
+            <div class="box3">
+                <h1> Zu verschenken </h1>
+            </div>
+            <div class="box1">
+                <img src="Bilder/profile/Lucas.jpeg" alt="Profil Bild">
                 <!-- Name -->
-                <p style="float: right; margin-top:75px; margin-left: 10px">Lucas Christoffers</p>
-                <div style=""><a href="mailto:">Nachricht</a></div>
+                <p>Lucas Christoffers</p>
+
+            </div>
+
+            <!-- Beschreibung -->
+            <div class="box2">
+                <p>
+                    Hey, dies ist mal wieder ein einfach quatsch Text, der ausgedacht worden ist. Denn damit soll
+                    einfach getestet werden, wie der Text tatsächlich aussieht und wie man damit herumspielen kann.
+                    Besonders wichtig ist es, sobald er mal ein klein wenig länger ausfällt. Aber irgendwie wird es
+                    immer schwerer sich quatsch auszudenken... vielleicht hätte ich einfach versuchen sollen einen
+                    richtigen Text zu schreiben, nun bin aber schon relativ weit hiermit und höre bestimmt nicht auf.
+                    Also setzen wir diesen sinnlosen Roman fort und dass nur für den simplen Grund, um längere Texte mal
+                    auszuprobieren.
+                </p>
+            </div>
+            <div class="box4">
+                <a href="mailto:">Nachricht</a>
             </div>
         </div>
     </div>
@@ -54,6 +54,8 @@
     <?php
     include "Ressourcen/footer.php";
     ?>
+
 </body>
+
 
 </html>
--- uebersicht.php
+++ uebersicht.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eigene Hilfsangebote</title>
+    <title>Document</title>
 
     <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="CSS/uebersicht.css">
     <link rel="stylesheet" type="text/css" href="CSS/style.css">
 </head>
 
@@ -16,28 +17,41 @@
     include "Ressourcen/header.php";
     ?>
     <!-- Content -->
-    <center>
-        <div>
-            <h3> Übersicht: </h3>
+
+    <div id="Container_Main_uebersicht">
+        <h3> Uebersicht: </h3>
+        <div class="Container_Liste">
+            <!-- Überschrift -->
+            <div class="ueberschrift">Nummer:</div>
+            <div class="ueberschrift">Titel:</div>
+            <div class="ueberschrift Kategorie_aktiv">Kategorie:</div>
+            <div class="ueberschrift">Loeschen:</div>
+            <div class="ueberschrift">Aendern:</div>
+            <!-- Listeneinträge -->
+            <!-- Angebot 1 -->
+            <div class="eintrag">1</div>
+            <div class="eintrag">abzugeben</div>
+            <div class="eintrag Kategorie_aktiv">abzugeben</div>
+            <div class="eintrag"><input type="checkbox" id="löschen" name="löschen" value="löschen">
+            </div>
+            <div class="eintrag"> <button type="button">Ändern </button></div>
+            <!-- Angebot 2 -->
+            <div class="eintrag">2</div>
+            <div class="eintrag">abzugeben</div>
+            <div class="eintrag Kategorie_aktiv">abzugeben</div>
+            <div class="eintrag"><input type="checkbox" id="löschen" name="löschen" value="löschen">
+            </div>
+            <div class="eintrag"> <button type="button">Ändern</button></div>
+            <!-- Angebot 3 -->
+            <div class="eintrag">3</div>
+            <div class="eintrag">abzugeben</div>
+            <div class="eintrag Kategorie_aktiv">abzugeben</div>
+            <div class="eintrag"><input type="checkbox" id="löschen" name="löschen" value="löschen">
+            </div>
+            <div class="eintrag"> <button type="button">Ändern </button> </div>
+
         </div>
-        <div>
-            <table style="border: 1px solid black; padding: 8px">
-                <tr>
-                    <th>Nummer: </th>
-                    <th>Titel: </th>
-                    <th>Kategorie: </th>
-                    <th>Löschen: </th>
-                </tr>
-                <tr>
-                    <th>1 </th>
-                    <th><a href="angebot_eins.php">zu verschenken</a> </th>
-                    <th> zu verschenken </th>
-                    <th><input type="checkbox"></th>
-                </tr>
-            </table>
-            <button type="button">Löschen</button>
-        </div>
-    </center>
+    </div>
     <!-- footer -->
     <?php
     include "Ressourcen/footer.php";
