6f2794007c4b0d709be3299cac2f658aea9d154c
Niviera <113305331+Niviera@users.noreply.github.com>
2023-05-31 16:03:23 +0200

    Update: Abgabe

---
+++ Datenbank/db_angebot_aendern.php
@@ -0,0 +1,21 @@
+<?php
+include "db_Verbindung.php";
+if (isset($_POST["ID"])) {
+    $query = "UPDATE Hilfsgesuch SET Titel= '".$_POST['titel']."',Beschreibung='".$_POST['beschreibung']."', Kategorie=".$_POST['kategorie']." WHERE ID = ".$_POST['ID'];
+    $query = $verbindung->prepare($query);
+    $query->execute();
+}
+
+$id;
+if (isset($_GET['ID'])) {
+    $id = $_GET['ID'];
+} else {
+    $id = $_POST['ID'];
+}
+$query = $verbindung->prepare("SELECT * FROM Hilfsgesuch WHERE Hilfsgesuch.ID = ?");
+$query->bindValue(1, $id);
+$query->execute();
+$ergebnis = $query->fetch();
+
+
+?>
