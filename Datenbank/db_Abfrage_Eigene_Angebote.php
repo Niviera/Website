118833b2f749f3aa7c2da74906902967ec0ffc68
Niviera <113305331+Niviera@users.noreply.github.com>
2023-06-06 18:17:51 +0200

    backup

--- Datenbank/db_Abfrage_Eigene_Angebote.php
+++ Datenbank/db_Abfrage_Eigene_Angebote.php
@@ -2,7 +2,8 @@
 include "db_Verbindung.php";
 
 /* Abfrage eigener Angebote */
-$query = $verbindung->prepare("SELECT Hilfsgesuch.ID as ID, Beschreibung,Titel ,Kategorie.Name FROM Hilfsgesuch LEFT JOIN Kategorie ON Hilfsgesuch.Kategorie = Kategorie.ID WHERE Ersteller = ?");
+$query = $verbindung->prepare("SELECT Hilfsgesuch.ID AS ID, Hilfsgesuch.Beschreibung, Hilfsgesuch.Titel, Kategorie.Name FROM Hilfsgesuch
+LEFT JOIN Kategorie ON Hilfsgesuch.Kategorie = Kategorie.ID WHERE Hilfsgesuch.Ersteller = ?");
 $query->bindValue(1, $_SESSION['UID']);
 
 $query->execute();
